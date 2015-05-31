8ca571cb428ee7a8c793885b7b33e9fa5618b07c
Alexander Reitzel <funtimecoding@gmail.com>
2015-05-31 15:06:04 +0200

    first commit

---
+++ create-db-dev.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+EXPECTED_ARGS=1
+E_BAD_ARGS=1
+
+if [ ${#} -ne ${EXPECTED_ARGS} ]; then
+    echo "Usage: ${0} NAME"
+    exit ${E_BAD_ARGS}
+fi
+
+./create-db.sh ${1} ${1} ${1}
---
+++ create-db.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+EXPECTED_ARGS=3
+E_BAD_ARGS=1
+MYSQL=$(which mysql)
+
+Q1="CREATE DATABASE IF NOT EXISTS ${1};"
+Q2="GRANT USAGE ON *.* TO ${2}@localhost IDENTIFIED BY '${3}';"
+Q3="GRANT ALL PRIVILEGES ON ${1}.* TO ${2}@localhost;"
+Q4="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}${Q3}${Q4}"
+
+if [ ${#} -ne ${EXPECTED_ARGS} ]; then
+    echo "Usage: ${0} DATABASE_NAME USERNAME PASSWORD"
+    exit ${E_BAD_ARGS}
+fi
+
+${MYSQL} -uroot -p -e "${SQL}"
