2b6d91da8a700bc4383e6ffbe420ddc17025c123
Alexander Reitzel <funtimecoding@gmail.com>
2015-12-04 12:32:53 +0100

    merge functionality of drop and delete script, rename scripts to not use abbreviations

---
+++ bin/delete-database.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -e
+
+usage()
+{
+    echo "Usage: ${0} [--with-user] DATABASE_NAME"
+}
+
+DATABASE_NAME="${1}"
+WITH_USER=false
+
+if [ "${1}" = "--with-user" ]; then
+    WITH_USER=true
+    shift
+fi
+
+if [ "${DATABASE_NAME}" = "" ]; then
+    usage
+
+    exit 1
+fi
+
+MYSQL=$(which mysql)
+DATABASE_SQL="DROP DATABASE IF EXISTS ${DATABASE_NAME};"
+
+if [ "${WITH_USER}" = true ]; then
+    USER_SQL="SET @users = NULL;
+    SELECT GROUP_CONCAT('\'', user, '\'@\'', host, '\'') INTO @users FROM mysql.user WHERE user='${DATABASE_NAME}';
+    SET @users = CONCAT('DROP USER ', @users);
+    PREPARE delete_statement FROM @users;
+    EXECUTE delete_statement;
+    DEALLOCATE PREPARE delete_statement;
+    DELETE FROM mysql.db WHERE Db LIKE '${DATABASE_NAME}';
+    FLUSH PRIVILEGES;"
+    SQL="${USER_SQL}${DATABASE_SQL}"
+else
+    SQL="${DATABASE_SQL}"
+fi
+
+${MYSQL} -uroot -p --protocol=tcp -e "${SQL}"
