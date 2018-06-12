5d4f00b174fe446ba7deb495fab2a3ce7ff6e9e5
Alexander Reitzel <funtimecoding@gmail.com>
2018-06-12 09:26:09 +0200

    install percona-toolkit

--- script/vagrant/provision.sh
+++ script/vagrant/provision.sh
@@ -10,6 +10,14 @@ if [ "${CODENAME}" = jessie ]; then
 elif [ "${CODENAME}" = stretch ]; then
     apt-get --quiet 2 install neovim multitail htop tree git shellcheck
 
+    # dirmngr is required to import the key
+    apt-get --quiet 2 install dirmngr
+    apt-key adv --keyserver keys.gnupg.net --recv-keys 8507EFA5
+    echo "deb http://repo.percona.com/apt stretch main" > /etc/apt/sources.list.d/percona-source.list
+    echo "deb-src http://repo.percona.com/apt stretch main" > /etc/apt/sources.list.d/percona-source.list
+    apt-get --quiet 2 update
+    apt-get --quiet 2 install percona-toolkit
+
     # TODO: How to install MySQL and not MariaDB? This still installs mariadb-server.
     #apt-get --quiet 2 install mysql-server
     apt-get --quiet 2 install mariadb-server
