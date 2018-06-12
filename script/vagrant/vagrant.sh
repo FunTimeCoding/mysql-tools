021252b356534e62044501fcb81d6c9d105292a3
Alexander Reitzel <funtimecoding@gmail.com>
2018-06-12 16:21:47 +0200

    semicolon not necessary here

--- script/vagrant/vagrant.sh
+++ script/vagrant/vagrant.sh
@@ -2,7 +2,7 @@
 
 bin/create-database.sh example
 bin/restore-database.sh example configuration/example.sql
-#bin/statement.sh example "GRANT ALL PRIVILEGES ON `example`.* TO 'example'@'%';"
+#bin/statement.sh example "GRANT ALL PRIVILEGES ON `example`.* TO 'example'@'%'"
 #bin/statement.sh example "SELECT * FROM guest"
 #bin/statement.sh example "DESC guest"
 echo "EXIT_CODE: $?"
