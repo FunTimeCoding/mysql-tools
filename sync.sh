e3806c8bf6d704e7a83e23dbcc447da2b56807ec
Alexander Reitzel <funtimecoding@gmail.com>
2018-06-12 17:04:25 +0200

    track exit code of pt-table-sync

--- sync.sh
+++ sync.sh
@@ -1,6 +1,14 @@
 #!/bin/sh -e
 
-pt-table-sync --execute --user root --password root h=localhost,D=example,t=guest h=localhost,D=example2,t=guest
+pt-table-sync --execute --user root --password root h=localhost,D=example,t=guest h=localhost,D=example2,t=guest && EXIT_CODE=$? || EXIT_CODE=$?
+
+if [ "${EXIT_CODE}" = 0 ]; then
+    echo "No difference."
+elif [ "${EXIT_CODE}" = 2 ]; then
+    echo "Synchronized."
+else
+    echo "Unexpected exit code: ${EXIT_CODE}"
+fi
 
 bin/statement.sh "SELECT * FROM guest;" example
 bin/statement.sh "SELECT * FROM guest;" example2
