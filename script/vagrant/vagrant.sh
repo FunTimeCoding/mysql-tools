2cbaf9c00ffc0104fe49c99c4dc95b120e3e839b
Alexander Reitzel <funtimecoding@gmail.com>
2018-06-12 16:40:07 +0200

    remove duplicate grant

--- script/vagrant/vagrant.sh
+++ script/vagrant/vagrant.sh
@@ -2,7 +2,6 @@
 
 bin/create-database.sh example
 bin/restore-database.sh example configuration/example.sql
-bin/statement.sh "GRANT ALL PRIVILEGES ON `example2`.* TO 'example'@'%'"
 #bin/statement.sh "SELECT * FROM guest" example
 #bin/statement.sh "DESC example.guest"
 echo "EXIT_CODE: $?"
