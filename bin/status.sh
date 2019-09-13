2dd87ceb581aff0a9d4b929e82dd269ad07724f7
Alexander Reitzel <funtimecoding@gmail.com>
2019-09-13 15:40:17 +0200

    pick specific status and variables

--- bin/status.sh
+++ bin/status.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -e
 
-bin/root-statement.sh "SHOW GLOBAL STATUS"
-# Aborted_clients
+bin/root-statement.sh "SHOW GLOBAL STATUS WHERE variable_name IN ('Aborted_clients', 'Bytes_received', 'Bytes_sent');"
 
-bin/root-statement.sh "SHOW VARIABLES"
+bin/root-statement.sh "SHOW VARIABLES WHERE variable_name IN ('wait_timeout', 'interactive_timeout', 'max_allowed_packet');"
 # wait_timeout default 28800, maybe double based on needs
 # interacive_timeout default 28800, maybe double based on needs
 # max_allowed_packet default 4mb, increase if application needs
